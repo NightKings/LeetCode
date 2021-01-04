620c910ecb2b62d8246c59641e95b2ce3112cece
NightKings <qtlkent@outlook.com>
2021-01-04 21:09:30 +0800

    两题

--- primaryCode/arrayalgo/main.c
+++ primaryCode/arrayalgo/main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 
 int removeDuplicates(int* nums, int numsSize);
 int maxProfit(int* prices, int pricesSize);
 void rotate(int* nums, int numsSize, int k);
 int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize);
+int* plusOne(int* digits, int digitsSize, int* returnSize);
+void moveZeroes(int* nums, int numsSize);
+
 
 int main() {
+    int nums1[]={9,0,7,6,5,0,0,2,1,0};
+    int nums2[]={9,9};
 
+    moveZeroes(nums1,10);
+    for (int i = 0; i < 10; ++i) {
+        printf("%d",nums1[i]);
+    }
 }
 //删除排序数组中重复项
 /**
@@ -66,11 +76,11 @@ void rotate(int* nums, int numsSize, int k){
  * 一种是排序后重复元素必然相邻
  * 一种是使用哈希表，表中同种元素只能有一个 set HashSet
  * **/
-int cmp(void* _a,void * _b){
-    int* a = (int*) _a;
-    int* b = (int*) _b;
-    return *a - *b;
+int cmp(const void* _a, const void* _b) {
+    int *a = (int*)_a, *b = (int*)_b;
+    return *a == *b ? 0 : *a > *b ? 1 : -1;
 }
+
 bool containsDuplicate(int* nums, int numsSize){
     if (numsSize<2){
         return false;
@@ -97,9 +107,72 @@ int singleNumber(int* nums, int numsSize){
 //两个数组的交集 II
 /**
  *重复的元素时，首先想到哈希表
+ * 两数组比较时，先排序后使用快慢指针
  * **/
+int min(int a ,int b){
+    if (a > b) return b;
+    return a;
+}
+
 int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize){
     qsort(nums1,nums1Size,sizeof(int),cmp);
     qsort(nums2,nums2Size, sizeof(int),cmp);
+    *returnSize = 0;
+    int* returnArray = (int*) malloc(sizeof(int)*min(nums1Size,nums2Size));
+    int up=0,down=0;
+    while (up < nums1Size && down < nums2Size){
+        if (nums1[up] > nums2[down]) {
+            down++;
+        }else if (nums1[up] == nums2[down]) {
+            returnArray[(*returnSize)++] = nums1[up];
+            up++;
+            down++;
+        } else up++;
+    }
+    return returnArray;
+}
 
+//加一
+/**
+ * 大的数不能直接定义出来
+ * **/
+int* plusOne(int* digits, int digitsSize, int* returnSize){
+    int carry = 1;
+    *returnSize = 0;
+    int* plusArray = (int*) malloc(sizeof(int)*(digitsSize+1));
+    for (int i = digitsSize - 1; i >= 0; --i) {
+        if (carry == 1){
+            if (digits[i] + 1 == 10){
+                carry = 1;
+            } else{
+                carry =0;
+            }
+            plusArray[(*returnSize)++] = (digits[i] + 1) % 10;
+        } else{
+            plusArray[(*returnSize)++] = digits[i];
+        }
+
+        if (i==0 & carry == 1){
+            plusArray[(*returnSize)++] = 1;
+        }
+    }
+    if ((*returnSize) == digitsSize){
+        reverse(plusArray,0,digitsSize);
+    } else{
+        reverse(plusArray,0,digitsSize+1);
+    }
+    return plusArray;
+}
+//移动零
+void moveZeroes(int* nums, int numsSize){
+    int j=0;
+    for (int i = 0; i <numsSize; ++i) {
+        if (nums[i]!=0){
+           nums[j] = nums[i];
+           j++;
+        }
+    }
+    for (int k = j; k < numsSize; ++k) {
+        nums[j] = 0;
+    }
 }
